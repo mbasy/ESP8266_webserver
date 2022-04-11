df991c8692d36ef378fc27b8996e5aaa7625ec89
unknown <mby@live.se>
2022-04-11 10:09:41 +0200

    Added header row is google sheets and fixed time to current zone

--- google_sheet.c
+++ google_sheet.c
@@ -1,171 +1,82 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include "HTTPSRedirect.h"
-#include <DHT.h>
+var SS = SpreadsheetApp.openById('1eJVNX6Q20rULhTGwSct2yz-587P4amM6uBMcmM-Nyi0');
+var str = "";
 
-#define DHTPIN 5                                                           // what digital pin we're connected to
-#define RIGHT_S 4                                                           // what digital pin we're connected right sensor to
-#define LEFT_S 14                                                           // what digital pin we're connected left sensor to
 
-#define DHTTYPE DHT22                                                       // select dht type as DHT 11 or DHT22
-DHT dht(DHTPIN, DHTTYPE);
+function doPost(e) {
 
-float h;
-float t;
-String sheetHumid = "";
-String sheetTemp = "";
-int people_count = 0;
-
-// Enter network credentials:
-const char* ssid     = "sigma-guest";
-const char* password = "starforlife2005";
-
-// Enter Google Script Deployment ID:
-const char *GScriptId = "AKfycbyd8zByoh1pEv8lf9VNWb9QASjXZEEwOeeNFkKv0bTm3NbEpvk";
-
-// Enter command (insert_row or append_row) and your Google Sheets sheet name (default is Sheet1):
-String payload_base =  "{\"command\": \"insert_row\", \"sheet_name\": \"TempSheet\", \"values\": ";
-String payload = "";
-
-// Google Sheets setup (do not edit)
-const char* host = "script.google.com";
-const int httpsPort = 443;
-const char* fingerprint = "";
-String url = String("/macros/s/") + GScriptId + "/exec";
-HTTPSRedirect* client = nullptr;
-
-// Declare variables that will be published to Google Sheets
-int value0 = 0;
-int value1 = 0;
-int value2 = 0;
-
-void setup() {
-  pinMode(RIGHT_S, INPUT);
-  pinMode(LEFT_S, INPUT);
-  Serial.begin(9600);        
-  delay(10);
-  Serial.println('\n');
-  dht.begin();     //initialise DHT22
+  var parsedData;
+  var result = {};
   
-  // Connect to WiFi
-  WiFi.begin(ssid, password);             
-  Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
+  try { 
+    parsedData = JSON.parse(e.postData.contents);
+  } 
+  catch(f){
+    return ContentService.createTextOutput("Error in parsing request body: " + f.message);
   }
-  Serial.println('\n');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());
-
-  // Use HTTPSRedirect class to create a new TLS connection
-  client = new HTTPSRedirect(httpsPort);
-  client->setInsecure();
-  client->setPrintResponseBody(true);
-  client->setContentTypeHeader("application/json");
-  
-  Serial.print("Connecting to ");
-  Serial.println(host);
-
-  // Try to connect for a maximum of 5 times
-  bool flag = false;
-  for (int i=0; i<5; i++){ 
-    int retval = client->connect(host, httpsPort);
-    if (retval == 1){
-       flag = true;
-       Serial.println("Connected");
-       break;
+   
+  if (parsedData !== undefined){
+    var flag = parsedData.format;
+    if (flag === undefined){
+      flag = 0;
     }
-    else
-      Serial.println("Connection failed. Retrying...");
+    
+    var sheet = SS.getSheetByName(parsedData.sheet_name); // sheet name to publish data to is specified in Arduino code
+    var dataArr = parsedData.values.split(","); // creates an array of the values to publish 
+         
+    var date_now = Utilities.formatDate(new Date(), "GMT+2", "dd/MM/yyyy"); // gets the current date
+    var time_now = Utilities.formatDate(new Date(), "GMT+2", "hh:mm:ss a"); // gets the current time
+    
+    var value0 = dataArr [0]; // value0 from Arduino code
+    var value1 = dataArr [1]; // value1 from Arduino code
+    var value2 = dataArr [2]; // value2 from Arduino code
+    
+    
+    // read and execute command from the "payload_base" string specified in Arduino code
+    switch (parsedData.command) {
+      
+      case "insert_row":
+         
+         sheet.insertRows(2); // insert full row directly below header text
+         
+         //var range = sheet.getRange("A2:D2");              // use this to insert cells just above the existing data instead of inserting an entire row
+         //range.insertCells(SpreadsheetApp.Dimension.ROWS); // use this to insert cells just above the existing data instead of inserting an entire row
+         sheet.getRange('A1').setValue("Date");
+         sheet.getRange('B1').setValue("Time");
+         sheet.getRange('C1').setValue("Humidity");
+         sheet.getRange('D1').setValue("Temperature");
+         sheet.getRange('A2').setValue(date_now); // publish current date to cell A2
+         sheet.getRange('B2').setValue(time_now); // publish current time to cell B2
+         sheet.getRange('C2').setValue(value0);   // publish value0 from Arduino code to cell C2
+         sheet.getRange('D2').setValue(value1);   // publish value1 from Arduino code to cell D2
+         sheet.getRange('E2').setValue(value2);   // publish value2 from Arduino code to cell E2
+         
+         str = "Success"; // string to return back to Arduino serial console
+         SpreadsheetApp.flush();
+         break;
+         
+      case "append_row":
+         
+         var publish_array = new Array(); // create a new array
+         
+         publish_array [0] = date_now; // add current date to position 0 in publish_array
+         publish_array [1] = time_now; // add current time to position 1 in publish_array
+         publish_array [2] = value0;   // add value0 from Arduino code to position 2 in publish_array
+         publish_array [3] = value1;   // add value1 from Arduino code to position 3 in publish_array
+         publish_array [4] = value2;   // add value2 from Arduino code to position 4 in publish_array
+         
+         sheet.appendRow(publish_array); // publish data in publish_array after the last row of data in the sheet
+         
+         str = "Success"; // string to return back to Arduino serial console
+         SpreadsheetApp.flush();
+         break;     
+ 
+    }
+    
+    return ContentService.createTextOutput(str);
+  } // endif (parsedData !== undefined)
+  
+  else {
+    return ContentService.createTextOutput("Error! Request body empty or in incorrect format.");
   }
-  if (!flag){
-    Serial.print("Could not connect to server: ");
-    Serial.println(host);
-    return;
-  }
-  delete client;    // delete HTTPSRedirect object
-  client = nullptr; // delete HTTPSRedirect object
 }
 
-
-void loop() {
-  int right_bubble = digitalRead(RIGHT_S);
-  int left_bubble = digitalRead(LEFT_S);
-  h = dht.readHumidity();                                              // Reading temperature or humidity takes about 250 milliseconds!
-  t = dht.readTemperature();                                           // Read temperature as Celsius (the default)
-
-  if (isnan(h) || isnan(t)) {                                                // Check if any reads failed and exit early (to try again).
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-
-if (right_bubble == HIGH){
-    Serial.println(" Motion Detected, right!\n");
-    delay(1000);
-    if(left_bubble == HIGH){
-                                         Serial.println("<<<<\n");
-                                         people_count++;
-                                         delay(3000);
-                                         }
-                            }
-                                       
-else if (left_bubble == HIGH){
-    Serial.println(" Motion Detected, left!\n");
-    delay(1000);
-    if(right_bubble == HIGH){
-                                         Serial.println(">>>>\n");
-                                         people_count--;
-                                         delay(3000);
-                                         }
-                            }
-              else{
-                   Serial.print("NO MOTION, # of people inside:%");
-                   Serial.print(people_count);
-                   Serial.println();
-                   
-              }
-              delay(500);
-
-  Serial.print("Humidity: ");  Serial.print(h);
-  sheetHumid = String(h) + String("%");                                         //convert integer humidity to string humidity
-  Serial.print("%  Temperature: ");  Serial.print(t);  Serial.println("°C ");
-  sheetTemp = String(t) + String("°C");
-
-  static bool flag = false;
-  if (!flag){
-    client = new HTTPSRedirect(httpsPort);
-    client->setInsecure();
-    flag = true;
-    client->setPrintResponseBody(true);
-    client->setContentTypeHeader("application/json");
-  }
-  if (client != nullptr){
-    if (!client->connected()){
-      client->connect(host, httpsPort);
-    }
-  }
-  else{
-    Serial.println("Error creating client object!");
-  }
-  
-  // Create json object string to send to Google Sheets
-  payload = payload_base + "\"" + sheetHumid + "," + sheetTemp + "\"}";
-  
-  // Publish data to Google Sheets
-  Serial.println("Publishing data...");
-  Serial.println(payload);
-  if(client->POST(url, host, payload)){ 
-    // do stuff here if publish was successful
-  }
-  else{
-    // do stuff here if publish was not successful
-    Serial.println("Error while connecting");
-  }
-
-  // a delay of several seconds is required before publishing again    
-  //delay(500);
-}
